79e5436d8d59a79bbf21674737d3d55b45da7bf7
Soyoung-Yi <spacenyi@naver.com>
2022-07-13 12:21:45 +0900

    07-13

---
+++ source/09_Spring/ch03_diLifeCycleConfig/src/main/java/com/lec/ch03/ex2/TestMain.java
@@ -0,0 +1,20 @@
+package com.lec.ch03.ex2;
+import org.springframework.context.support.GenericXmlApplicationContext;
+public class TestMain {
+	public static void main(String[] args) {
+		// IOC 컨테이너 생성(환경설정)
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		// 파싱
+		ctx.load("classpath:META-INF/ex2/applicationCTX.xml");
+		ctx.refresh(); // 빈생성
+		System.out.println("-----------------------------------");
+		Person person = ctx.getBean("person", Person.class);
+		System.out.println(person);
+		OtherPerson otherPerson = (OtherPerson)ctx.getBean("otherPerson");
+		System.out.println(otherPerson);
+		System.out.println("-----------------------------------");
+		ctx.close();
+		System.out.println("빈 소멸");
+	}
+
+}
