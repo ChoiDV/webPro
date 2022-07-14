9347c3e692eb4c838c2bef73943edf79378d5067
Soyoung-Yi <spacenyi@naver.com>
2022-07-14 16:25:29 +0900

    07-14

--- source/09_Spring/ch07_controller/src/main/java/com/lec/ch07/controller/BoardController.java
+++ source/09_Spring/ch07_controller/src/main/java/com/lec/ch07/controller/BoardController.java
@@ -1,8 +1,13 @@
 package com.lec.ch07.controller;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-// board/write, board/content, board/list, board/reply
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lec.ch07.dto.Member;
+// board/write, board/content, board/list, board/reply
 @Controller
 @RequestMapping("board") // 공통요청경로
 public class BoardController {
@@ -10,8 +15,18 @@ public class BoardController {
 	public String write() {
 		return "board/write"; // 뷰 : /WEB-INF/views/board/write.jsp
 	}
-	
+	@RequestMapping(value="content", method=RequestMethod.GET)
+	public String content(Model model/*, HttpServletRequest request*/) {
+		// request.setAttribute("id", "aaa");
+		model.addAttribute("id", "aaa");
+		model.addAttribute("pw", "111");
+		Member member = new Member("bbb", "222");
+		model.addAttribute("member", member);
+		return "board/content";
+	}
 }
+
+
 
 
 
