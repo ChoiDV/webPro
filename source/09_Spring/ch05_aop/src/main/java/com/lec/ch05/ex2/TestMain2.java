474925b3c36b1b58b3d19f47292b2eaa8a978730
Soyoung-Yi <spacenyi@naver.com>
2022-07-14 12:25:15 +0900

    07-14

--- source/09_Spring/ch05_aop/src/main/java/com/lec/ch05/ex2/TestMain2.java
+++ source/09_Spring/ch05_aop/src/main/java/com/lec/ch05/ex2/TestMain2.java
@@ -2,10 +2,10 @@ package com.lec.ch05.ex2;
 import org.springframework.context.support.GenericXmlApplicationContext;
 public class TestMain2 {
 	public static void main(String[] args) {
-		String location = "classpath:META-INF/applicationCTX1.xml";
+		String location = "classpath:META-INF/applicationCTX2.xml";
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext(location);
-		Student2 student = ctx.getBean("student", Student2.class);
-		Worker2  worker  = ctx.getBean("worker", Worker2.class);
+		Student2 student = ctx.getBean("student2", Student2.class);
+		Worker2  worker  = ctx.getBean("worker2", Worker2.class);
 		student.getStudentInfo(); // 핵심기능
 		System.out.println("------------------------------");
 		worker.getWorkerInfo();   // 핵심기능
