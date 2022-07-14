4309ca939fab269ef0b35b49014f0898fecf8766
Soyoung-Yi <spacenyi@naver.com>
2022-07-14 17:21:50 +0900

    07-14

--- source/09_Spring/ch07_controller/src/main/java/com/lec/ch07/controller/BoardController.java
+++ source/09_Spring/ch07_controller/src/main/java/com/lec/ch07/controller/BoardController.java
@@ -1,10 +1,13 @@
 package com.lec.ch07.controller;
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.lec.ch07.dto.Member;
 // board/write, board/content, board/list, board/reply
@@ -23,6 +26,23 @@ public class BoardController {
 		Member member = new Member("bbb", "222");
 		model.addAttribute("member", member);
 		return "board/content";
+	}
+	@RequestMapping(value="list", method=RequestMethod.GET)
+	public ModelAndView list() {
+		ModelAndView mav = new ModelAndView();
+		Member member = new Member("ccc", "111");
+		ArrayList<Member> list = new ArrayList<Member>();
+		list.add(member);
+		mav.addObject("list", list); // model.addAttribute("list", list);
+		mav.setViewName("board/list"); // 뷰
+		return mav;
+	}
+	@RequestMapping(value="reply", method=RequestMethod.GET)
+	public ModelAndView reply(ModelAndView mav) {
+		Member member = new Member("ddd", "111");
+		mav.addObject("member", member);
+		mav.setViewName("board/reply");
+		return mav;
 	}
 }
 
