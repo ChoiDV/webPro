cc670ebda12af757eec317c51108b6b55dd13092
Soyoung-Yi <spacenyi@naver.com>
2022-07-19 12:20:49 +0900

    07-19

--- source/09_Spring/ch12_jdbcTemplate/src/main/java/com/lec/ch12/dao/BoardDao.java
+++ source/09_Spring/ch12_jdbcTemplate/src/main/java/com/lec/ch12/dao/BoardDao.java
@@ -29,17 +29,6 @@ public class BoardDao {
 	private BoardDao() {
 		template = Constant.template;
 	}
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-		try {
-			Context ctx = new InitialContext();
-			DataSource ds = (DataSource)ctx.lookup("java:comp/env/jdbc/Oracle11g");
-			conn = ds.getConnection();
-		} catch (NamingException e) {
-			System.out.println(e.getMessage());
-		}
-		return conn;
-	}
 	// 글목록
 	public ArrayList<BoardDto> boardList(){
 		String sql = "SELECT * FROM MVC_BOARD ORDER BY BGROUP DESC, BSTEP";
@@ -100,115 +89,60 @@ public class BoardDao {
 		return template.queryForObject(sql, new BeanPropertyRowMapper<BoardDto>(BoardDto.class));
 	}
 	// 글 수정하기
-	public int modify(BoardDto bDto) {
-		int result = FAIL;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
+	public int modify(final BoardDto bDto) {
 		String sql = "UPDATE MVC_BOARD SET BNAME=?, " + 
 				"                    BTITLE = ?, " + 
 				"                    BCONTENT = ?, " + 
 				"                    BIP = ?" + 
 				"        WHERE BID=?";
-		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, bDto.getBname());
-			pstmt.setString(2, bDto.getBtitle());
-			pstmt.setString(3, bDto.getBcontent());
-			pstmt.setString(4, bDto.getBip());
-			pstmt.setInt(5, bDto.getBid());
-			result = pstmt.executeUpdate();
-			System.out.println(result==SUCCESS ? "글수정 성공":"글수정 실패");
-		} catch (SQLException e) {
-			System.out.println("글수정 실패 예외 : "+e.getMessage());
-		}finally {
-			try {
-				if(pstmt!=null) pstmt.close();
-				if(conn!=null) conn.close();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
+		return template.update(sql, new PreparedStatementSetter() {
+			
+			@Override
+			public void setValues(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, bDto.getBname());
+				pstmt.setString(2, bDto.getBtitle());
+				pstmt.setString(3, bDto.getBcontent());
+				pstmt.setString(4, bDto.getBip());
+				pstmt.setInt(5, bDto.getBid());				
 			}
-		}
-		return result;
+		});
 	}
 	// 글 삭제
 	public int delete(int bid) {
-		int result = FAIL;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		String sql = "DELETE FROM MVC_BOARD WHERE BID=?";
-		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, bid);
-			result = pstmt.executeUpdate();
-			System.out.println(result==SUCCESS? bid+"번 글삭제 성공":bid+"번글 삭제실패");
-		} catch (SQLException e) {
-			System.out.println("글삭제 예외 : "+e.getMessage());
-		}finally {
-			try {
-				if(pstmt!=null) pstmt.close();
-				if(conn!=null) conn.close();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
-			}
-		}
-		return result;
+		String sql = "DELETE FROM MVC_BOARD WHERE BID="+bid;
+		return template.update(sql);
 	}
 	// 답변글 쓰기 전 작업 : stepA
-	private void preReplyStepA(int bgroup, int bstep) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
+	private void preReplyStepA(final int bgroup, final int bstep) {
 		String sql = "UPDATE MVC_BOARD SET BSTEP = BSTEP+1 WHERE BGROUP=? AND BSTEP>?";
-		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, bgroup);
-			pstmt.setInt(2, bstep);
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("답변글 쓰기 전 작업 예외 : "+e.getMessage());
-		}finally {
-			try {
-				if(pstmt!=null) pstmt.close();
-				if(conn!=null) conn.close();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
+		template.update(sql, new PreparedStatementSetter() {
+			
+			@Override
+			public void setValues(PreparedStatement pstmt) throws SQLException {
+				pstmt.setInt(1, bgroup);
+				pstmt.setInt(2, bstep);
 			}
-		}
+		});
 	}
 	// 답변글 쓰기
-	public int reply(BoardDto bDto) { // bname, btitle, bcontent, bip : 답변글에 대한 정보
+	public int reply(final BoardDto bDto) { // bname, btitle, bcontent, bip : 답변글에 대한 정보
 		                             // bgroup, bstep, bindent : 원글에 대한 정보
 		preReplyStepA(bDto.getBgroup(), bDto.getBstep());
-		int result = FAIL;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
 		String sql = "INSERT INTO MVC_BOARD" + 
 				" (BID, BNAME, BTITLE, BCONTENT, BGROUP, BSTEP, BINDENT, BIP) " + 
 				"    VALUES (MVC_BOARD_SEQ.NEXTVAL, ?,?,?, ?, ?, ?, ?)";
-		try {
-			conn = getConnection();
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, bDto.getBname());
-			pstmt.setString(2, bDto.getBtitle());
-			pstmt.setString(3, bDto.getBcontent());
-			pstmt.setInt(4, bDto.getBgroup());
-			pstmt.setInt(5, bDto.getBstep()+1);
-			pstmt.setInt(6, bDto.getBindent()+1);
-			pstmt.setString(7, bDto.getBip());
-			result = pstmt.executeUpdate();
-			System.out.println(result==SUCCESS ? "답변쓰기 성공":"답변쓰기 실패");
-		} catch (SQLException e) {
-			System.out.println("답변쓰기 실패 예외 : "+e.getMessage());
-		}finally {
-			try {
-				if(pstmt!=null) pstmt.close();
-				if(conn!=null) conn.close();
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
+		return template.update(sql, new PreparedStatementSetter() {
+			
+			@Override
+			public void setValues(PreparedStatement pstmt) throws SQLException {
+				pstmt.setString(1, bDto.getBname());
+				pstmt.setString(2, bDto.getBtitle());
+				pstmt.setString(3, bDto.getBcontent());
+				pstmt.setInt(4, bDto.getBgroup());
+				pstmt.setInt(5, bDto.getBstep()+1);
+				pstmt.setInt(6, bDto.getBindent()+1);
+				pstmt.setString(7, bDto.getBip());
 			}
-		}
-		return result;
+		});
 	}
 }
