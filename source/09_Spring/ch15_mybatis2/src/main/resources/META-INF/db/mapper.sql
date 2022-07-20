08af94ef58324cc5644f16d64e2f6e8181db6410
Soyoung-Yi <spacenyi@naver.com>
2022-07-20 17:01:13 +0900

    07-19

--- source/09_Spring/ch15_mybatis2/src/main/resources/META-INF/db/mapper.sql
+++ source/09_Spring/ch15_mybatis2/src/main/resources/META-INF/db/mapper.sql
@@ -10,6 +10,25 @@ SELECT COUNT(*) FROM EMP;
 SELECT * FROM EMP WHERE EMPNO=7698;
 -- Emp.xml id=managerList
 SELECT * FROM EMP WHERE EMPNO IN (SELECT MGR FROM EMP);
+-- Emp.xml id=insert
+INSERT INTO EMP VALUES (9000, '홍길동','IT', 7902, '22/07/20', 6000, 800, 40);
+-- Emp.xml id=update
+UPDATE EMP SET ENAME='홍홍홍',
+            JOB='MANAGER',
+            MGR=7839,
+            HIREDATE='21/07/20',
+            SAL = 7000,
+            COMM = 1000,
+            DEPTNO=20
+        WHERE EMPNO=9000;
+-- Emp.xml id=delete
+DELETE FROM EMP WHERE EMPNO=9000;
+
+-- EmpDept.xml id=empDeptList
+-- SELECT E.*, DNAME, LOC FROM EMP E, DEPT D WHERE E.DEPTNO=D.DEPTNO ORDER BY EMPNO; 출력 조건
+SELECT * FROM (SELECT ROWNUM RN, A.* FROM 
+        (SELECT E.*, DNAME, LOC FROM EMP E, DEPT D WHERE E.DEPTNO=D.DEPTNO ORDER BY EMPNO) A)
+    WHERE RN BETWEEN 6 AND 10;
 
 
 
