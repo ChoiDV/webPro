f8a40180b06b6580e3ec8e9cf3acbf15b840d68e
Soyoung-Yi <spacenyi@naver.com>
2022-07-21 09:47:23 +0900

    07-21

--- source/09_Spring/ch15_mybatis2/src/main/resources/META-INF/db/mapper.sql
+++ source/09_Spring/ch15_mybatis2/src/main/resources/META-INF/db/mapper.sql
@@ -1,18 +1,23 @@
---  Dept.xml id=deptList
+--  ★ Dept.xml id=deptList
 SELECT * FROM DEPT;
--- Emp.xml id=empList
+-- ☆ Emp.xml id=empList
 -- SELECT * FROM EMP ORDER BY EMPNO; -- 출력 조건
 SELECT * FROM (SELECT ROWNUM RN, A.* FROM (SELECT * FROM EMP ORDER BY EMPNO) A)
     WHERE RN BETWEEN 6 AND 10;
--- Emp.xml id=totCnt
+-- ☆ Emp.xml id=empDeptList
+-- SELECT E.*, DNAME, LOC FROM EMP E, DEPT D WHERE E.DEPTNO=D.DEPTNO ORDER BY EMPNO; 출력 조건
+SELECT * FROM (SELECT ROWNUM RN, A.* FROM 
+        (SELECT E.*, DNAME, LOC FROM EMP E, DEPT D WHERE E.DEPTNO=D.DEPTNO ORDER BY EMPNO) A)
+    WHERE RN BETWEEN 6 AND 10;
+-- ☆ Emp.xml id=totCnt
 SELECT COUNT(*) FROM EMP;
--- Emp.xml id=detail
+-- ☆ Emp.xml id=detail
 SELECT * FROM EMP WHERE EMPNO=7698;
--- Emp.xml id=managerList
+-- ☆ Emp.xml id=managerList
 SELECT * FROM EMP WHERE EMPNO IN (SELECT MGR FROM EMP);
--- Emp.xml id=insert
+-- ☆ Emp.xml id=insert
 INSERT INTO EMP VALUES (9000, '홍길동','IT', 7902, '22/07/20', 6000, 800, 40);
--- Emp.xml id=update
+-- ☆ Emp.xml id=update
 UPDATE EMP SET ENAME='홍홍홍',
             JOB='MANAGER',
             MGR=7839,
@@ -21,14 +26,8 @@ UPDATE EMP SET ENAME='홍홍홍',
             COMM = 1000,
             DEPTNO=20
         WHERE EMPNO=9000;
--- Emp.xml id=delete
+-- ☆ Emp.xml id=delete
 DELETE FROM EMP WHERE EMPNO=9000;
-
--- EmpDept.xml id=empDeptList
--- SELECT E.*, DNAME, LOC FROM EMP E, DEPT D WHERE E.DEPTNO=D.DEPTNO ORDER BY EMPNO; 출력 조건
-SELECT * FROM (SELECT ROWNUM RN, A.* FROM 
-        (SELECT E.*, DNAME, LOC FROM EMP E, DEPT D WHERE E.DEPTNO=D.DEPTNO ORDER BY EMPNO) A)
-    WHERE RN BETWEEN 6 AND 10;
 
 
 
