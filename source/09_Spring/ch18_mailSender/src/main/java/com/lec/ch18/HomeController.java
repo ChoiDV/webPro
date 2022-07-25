a3fc832eaf94f574d7b53b2aa02f8cd8cce88062
Soyoung-Yi <spacenyi@naver.com>
2022-07-25 11:28:57 +0900

    07-25

--- source/09_Spring/ch18_mailSender/src/main/java/com/lec/ch18/HomeController.java
+++ source/09_Spring/ch18_mailSender/src/main/java/com/lec/ch18/HomeController.java
@@ -4,11 +4,16 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.mail.Message;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,9 +41,36 @@ public class HomeController {
 		return "sendResult";
 	}
 	@RequestMapping(value = "htmlMail", method = RequestMethod.GET)
-	public String htmlMail(String name, String email, Model model) {
+	public String htmlMail(final String name, final String email, Model model) {
 		// name에게 email 주소로 html 내용 보내기
-		
+		MimeMessagePreparator message = new MimeMessagePreparator() {
+			// 메일 내용(html)
+			String content = "<div style=\"width:500px; margin: 0 auto; text-align: center\">\n" + 
+					"	<h1 style=\"color:blue;\">"+ name +"님 회원가입 감사합니다</h1>\n" + 
+					"	<div>\n" + 
+					"		<p>아무개 사이트에서만 쓰실 수 있는 감사쿠폰을 드립니다</p>\n" + 
+					"		<p>\n" + 
+					"			<img src=\"http://localhost:8090/ch18/resources/coupon.jpg\">\n" + 
+					"		</p>\n" + 
+					"	</div>\n" + 
+					"	<div>\n" + 
+					"		<p style=\"color:red;\">빨간 글씨 부분</p>\n" + 
+					"		<p style=\"color:blue;\">파란 글씨 부분</p>\n" + 
+					"		<img src=\"https://t1.daumcdn.net/daumtop_chanel/op/20200723055344399.png\">\n" + 
+					"	</div>\n" + 
+					"	<p>서울시 어떤구 XX로 00 **빌딩 402</p>\n" + 
+					"</div>"; 
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				// 보내는 메일, 받는 메일, 메일 제목, utf-8
+				mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
+				mimeMessage.setFrom(new InternetAddress("spacenyi0@gmail.com"));
+				mimeMessage.setSubject("[HTML가입인사] "+ name + "님 회원가입 감사합니다");
+				mimeMessage.setText(content, "utf-8", "html");
+			}
+		};
+		mailSender.send(message);
+		model.addAttribute("mailSendResult", "HTML 메일이 발송되었습니다");
 		return "sendResult";
 	}
 }
