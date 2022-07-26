a9fa53bd0bd048438c882678a214b955c8b64fdd
Soyoung-Yi <spacenyi@naver.com>
2022-07-26 10:10:18 +0900

    07-26

--- source/09_Spring/ch19_memberBookUp/src/main/java/com/lec/ch19/controller/MemberController.java
+++ source/09_Spring/ch19_memberBookUp/src/main/java/com/lec/ch19/controller/MemberController.java
@@ -1,10 +1,14 @@
 package com.lec.ch19.controller;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.lec.ch19.dto.Member;
 import com.lec.ch19.service.MemberService;
 @Controller
 @RequestMapping(value="member")
@@ -17,6 +21,22 @@ public class MemberController {
 	}
 	@RequestMapping(params = "method=idConfirm", method = RequestMethod.GET)
 	public String idconfirm(String mid, Model model) {
-		
+		model.addAttribute("result", memberService.idConfirm(mid));
+		return "member/idConfirm";
+	}
+	@RequestMapping(params = "method=join", method = RequestMethod.POST)
+	public String join(@ModelAttribute("mDto") Member member, HttpSession httpSession, Model model) {
+		model.addAttribute("joinResult", memberService.joinMember(member, httpSession));
+		return "forward:member.do?method=loginForm";
+	}
+	@RequestMapping(params = "method=loginForm", method = {RequestMethod.GET, RequestMethod.POST})
+	public String loginForm() {
+		return "member/loginForm";
 	}
 }
+
+
+
+
+
+
