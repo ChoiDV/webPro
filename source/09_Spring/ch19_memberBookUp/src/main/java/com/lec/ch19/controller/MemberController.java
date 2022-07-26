1af76c52ea354d1b3ff78aa5ade5ee133d8ec42d
Soyoung-Yi <spacenyi@naver.com>
2022-07-26 09:51:32 +0900

    07-26

---
+++ source/09_Spring/ch19_memberBookUp/src/main/java/com/lec/ch19/controller/MemberController.java
@@ -0,0 +1,17 @@
+package com.lec.ch19.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.lec.ch19.service.MemberService;
+@Controller
+@RequestMapping(value="member")
+public class MemberController {
+	@Autowired
+	private MemberService memberService;
+	@RequestMapping(params = "method=joinForm", method = RequestMethod.GET)
+	public String joinForm() {
+		return "member/joinForm";
+	}
+}
