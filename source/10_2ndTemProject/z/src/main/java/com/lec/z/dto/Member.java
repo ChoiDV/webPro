a25756bf056a462c18c655bab47ec68a11a0ad39
Soyoung-Yi <spacenyi@naver.com>
2022-07-27 16:37:28 +0900

    07-27

---
+++ source/10_2ndTemProject/z/src/main/java/com/lec/z/dto/Member.java
@@ -0,0 +1,11 @@
+package com.lec.z.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Member {
+	private int num;
+	private String name;
+}
---
+++ source/10_2ndTemProject/z/src/main/java/com/lec/z/dto/Test.java
@@ -0,0 +1,10 @@
+package com.lec.z.dto;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Member mem = new Member(99, "홍길동");
+
+	}
+
+}
