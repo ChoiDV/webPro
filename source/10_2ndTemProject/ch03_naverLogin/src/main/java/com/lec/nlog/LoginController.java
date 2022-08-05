49d51b37110091c6c808a29202ce13471a1c7c8e
Soyoung-Yi <spacenyi@naver.com>
2022-08-05 16:21:00 +0900

    08-05

--- source/10_2ndTemProject/ch03_naverLogin/src/main/java/com/lec/nlog/LoginController.java
+++ source/10_2ndTemProject/ch03_naverLogin/src/main/java/com/lec/nlog/LoginController.java
@@ -15,68 +15,72 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.github.scribejava.core.model.OAuth2AccessToken;
+
 @Controller
 public class LoginController {
 	/* NaverLoginBO */
 	private NaverLoginBO naverLoginBO;
 	private String apiResult = null;
+
 	@Autowired
 	private void setNaverLoginBO(NaverLoginBO naverLoginBO) {
 		this.naverLoginBO = naverLoginBO;
 	}
-	//로그인 첫 화면 요청 메소드
+
+	// 로그인 첫 화면 요청 메소드
 	@RequestMapping(value = "/login", method = { RequestMethod.GET, RequestMethod.POST })
 	public String login(Model model, HttpSession session) {
-		
+
 		/* 네이버아이디로 인증 URL을 생성하기 위하여 naverLoginBO클래스의 getAuthorizationUrl메소드 호출 */
 		String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
-		
+
 		System.out.println("네이버:" + naverAuthUrl);
-		
-		//네이버 
+
+		// 네이버
 		model.addAttribute("url", naverAuthUrl);
-		//개발자센터 > Application > 내 애플리케이션'의 '네아로 검수 상태' 탭에서 검수를 요청을 해야 검수가 승인되고 제약 없이 모든 아이디에서 네아로 이용 가능. 그 전엔 개발자 아이디로만 가능
+		// 개발자센터 > Application > 내 애플리케이션'의 '네아로 검수 상태' 탭에서 검수를 요청을 해야 검수가 승인되고 제약 없이 모든
+		// 아이디에서 네아로 이용 가능. 그 전엔 개발자 아이디로만 가능
 		return "login";
 	}
-	//네이버 로그인 성공시 callback호출 메소드
-		@RequestMapping(value = "/callback", method = { RequestMethod.GET, RequestMethod.POST })
-		public String callback(Model model, @RequestParam String code, @RequestParam String state, HttpSession session) throws IOException, ParseException {
-			
-			OAuth2AccessToken oauthToken;
-	        oauthToken = naverLoginBO.getAccessToken(session, code, state);
-			apiResult = naverLoginBO.getUserProfile(oauthToken);  //String형식의 json데이터
-			
-			//2. String형식인 apiResult를 json형태로 바꿈
-			JSONParser parser = new JSONParser();
-			Object obj = parser.parse(apiResult);
-			JSONObject jsonObj = (JSONObject) obj;
-			
-			//3. 데이터 파싱 
-			//Top레벨 단계 _response 파싱
-			JSONObject response_obj = (JSONObject)jsonObj.get("response");
-			//response의 nickname값 파싱
-			String nickname = (String)response_obj.get("nickname");
-			String name = (String)response_obj.get("name");
-			String id = (String)response_obj.get("id"); // API 호출 결과로 네이버 아이디값은 제공하지 않으며, 대신 'id'라는 애플리케이션당 유니크한 일련번호값을 이용해서 자체적으로 회원정보를 구성하셔야 합니다.
-			
-			//4.파싱 닉네임 세션으로 저장
-			session.setAttribute("sessionId",name+"("+nickname+" ; "+id+")"); //세션 생성
-			
-			model.addAttribute("result", apiResult);
-			System.out.println("name : " + name);
-		    System.out.println("nickname : " + nickname);
-		    System.out.println("apiResult : " + apiResult);
-			return "login";
-		}
-		
-		//로그아웃
-		@RequestMapping(value = "/logout", method = { RequestMethod.GET, RequestMethod.POST })
-		public String logout(HttpSession session)throws IOException {
-				System.out.println("여기는 logout");
-				session.invalidate();
-	 
-		        
-				return "redirect:index.jsp";
-			}
+
+	// 네이버 로그인 성공시 callback호출 메소드
+	@RequestMapping(value = "/callback", method = { RequestMethod.GET, RequestMethod.POST })
+	public String callback(Model model, @RequestParam String code, @RequestParam String state, HttpSession session)
+			throws IOException, ParseException {
+
+		OAuth2AccessToken oauthToken;
+		oauthToken = naverLoginBO.getAccessToken(session, code, state);
+		apiResult = naverLoginBO.getUserProfile(oauthToken); // String형식의 json데이터
+
+		// 2. String형식인 apiResult를 json형태로 바꿈
+		JSONParser parser = new JSONParser();
+		Object obj = parser.parse(apiResult);
+		JSONObject jsonObj = (JSONObject) obj;
+
+		// 3. 데이터 파싱
+		// Top레벨 단계 _response 파싱
+		JSONObject response_obj = (JSONObject) jsonObj.get("response");
+		// response의 nickname값 파싱
+		String nickname = (String) response_obj.get("nickname");
+		String name = (String) response_obj.get("name");
+		String id = (String) response_obj.get("id"); // API 호출 결과로 네이버 아이디값은 제공하지 않으며, 대신 'id'라는 애플리케이션당 유니크한 일련번호값을
+														// 이용해서 자체적으로 회원정보를 구성하셔야 합니다.
+
+		// 4.파싱 닉네임 세션으로 저장
+		session.setAttribute("sessionId", name + "(" + nickname + " ; " + id + ")"); // 세션 생성
+
+		model.addAttribute("result", apiResult);
+		System.out.println("name : " + name);
+		System.out.println("nickname : " + nickname);
+		System.out.println("apiResult : " + apiResult);
+		return "login";
+	}
+
+	// 로그아웃
+	@RequestMapping(value = "/logout", method = { RequestMethod.GET, RequestMethod.POST })
+	public String logout(HttpSession session) throws IOException {
+		session.invalidate();
+		return "redirect:index.jsp";
+	}
 
 }
